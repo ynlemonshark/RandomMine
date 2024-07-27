4eba938af88380482e554b21a9da5fe66c056881
lemonshark <lemonsharksea@gmail.com>
2024-07-28 00:23:22 +0900

    Make Rock vibrate

--- RandomMine.py
+++ RandomMine.py
@@ -1,7 +1,7 @@
 # RandomMine pygame file
 import pygame
 import sys
-from pygame.locals import QUIT, Rect
+from pygame.locals import QUIT, Rect, MOUSEBUTTONDOWN
 
 Display_width = 1200
 Display_height = 800
@@ -20,28 +20,54 @@ SURFACE = pygame.Surface((Surface_width, Surface_height))
 FPSCLOCK = pygame.time.Clock()
 
 # Rock
-Rock_rect = Rect(0, 0, 394, 264)
+Rock_rect = Rect(0, 0, 336, 231)
 Rock_rect.center = (Surface_width / 2, Surface_height / 2)
 
 Rock_image = pygame.transform.scale(pygame.image.load("resources/Rock.png"), Rock_rect.size)
 
+rock_vibration_power = 10
+rock_vibration_delay = 50
+
 
 def main():
+    tickTime = 1000 / FPS
+    vibrationTick = 0
+
     while True:
+
         pygame_events = pygame.event.get()
         for pygame_event in pygame_events:
+            # exit code
             if pygame_event.type == QUIT:
                 pygame.quit()
                 sys.exit()
 
+            # event mouse position set
+            elif pygame_event.type == MOUSEBUTTONDOWN:
+                event_pos = (pygame_event.pos[0] / display_ratio_x,
+                             pygame_event.pos[1] / display_ratio_y)
+
+                # mining
+                if Rock_rect.collidepoint(event_pos):
+                    vibrationTick = rock_vibration_delay
+
+        # surface set
         SURFACE.fill((255, 0, 0))
 
-        SURFACE.blit(Rock_image, Rock_rect.topleft)
+        # rock vibration
+        if vibrationTick:
+            SURFACE.blit(Rock_image, (Rock_rect.left + rock_vibration_power, Rock_rect.top))
+            vibrationTick = max(vibrationTick - tickTime, 0)
+        else:
+            SURFACE.blit(Rock_image, Rock_rect.topleft)
 
+        # system
         DISPLAY.blit(pygame.transform.scale(SURFACE, (Display_width, Display_height)), (0, 0))
 
         pygame.display.update()
         FPSCLOCK.tick(FPS)
+
+        tickTime = FPSCLOCK.get_time()
 
 
 if __name__ == "__main__":
